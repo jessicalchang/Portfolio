5d79a050d0783c4de8fe20ee7022ef608b8bd922
Nikitronus <Slimehide@gmail.com>
2024-04-13 15:12:47 +0300

    new website

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	
+
+	let totalWidth = $('.top__dash>.info').outerWidth() + 24;
+	$('.top__dash>.elem').each(function(index,elem){
+		totalWidth = +totalWidth + +$(elem).outerWidth() + 24;
+	});
+	totalWidth = totalWidth + 63;
+
+	let visiblePart = totalWidth - $(window).width();
+	$(window).on('resize' ,function(){
+		totalWidth = $('.top__dash>.info').outerWidth() + 24;
+		$('.top__dash>.elem').each(function(index,elem){
+			totalWidth = +totalWidth + +$(elem).outerWidth() + 24;
+		});
+		totalWidth = totalWidth + 63;
+		visiblePart = totalWidth - $(window).width();
+		$('.container').css('height' , visiblePart + $(window).height());
+	});
+	$('.container').css('height' , visiblePart + $(window).height());
+	$(window).on("scroll" ,function(){
+		$('.top__dash').css('transform' ,"translateX(-"+ $(window).scrollTop() +"px)")
+	});
+
+	if ($(window).width() < 1280) {
+		$('.top__dash>.elem.back').css("height" , $('.top__dash>.elem:nth-child(4)').outerHeight());
+		
+	}
+	$(window).on("resize" ,function(){
+		if ($(window).width() < 1280) {
+			$('.top__dash>.elem.back').css("height" , $('.top__dash>.elem:nth-child(4)').outerHeight());
+		} else {
+			$('.top__dash>.elem.back').css("height" , "auto");
+		}
+	});
+});
